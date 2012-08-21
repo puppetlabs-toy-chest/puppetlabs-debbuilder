7efcaea388c7b589e3d5c5938ceaf947bf480ada
Matthaus Litteken <matthaus@puppetlabs.com>
2012-08-21 15:31:56 -0700

    Lucid shipped with git-core, not git, so this commit adds some special case logic to handle ubuntu's special case.

--- manifests/packages/essential.pp
+++ manifests/packages/essential.pp
@@ -13,15 +13,19 @@ class debbuilder::packages::essential {
     "cdbs",
     "build-essential",
     "rake",
-    "git",
     "pristine-tar",
   ]
+
+  case $::lsbdistcodename {
+    /lucid/:  { $git = "git-core" }
+    default:  { $git = "git" }
+  }
 
   case $::lsbdistcodename {
     /(wheezy|sid|testing|quantal)/: { $rspec = "ruby-rspec" }
     default:                        { $rspec = "librspec-ruby" }
   }
 
-  package { [$builder_packages, $rspec]: ensure => present, }
+  package { [$builder_packages, $rspec, $git]: ensure => present, }
 
 }
--- spec/classes/packages_essential_spec.rb
+++ spec/classes/packages_essential_spec.rb
@@ -10,20 +10,19 @@ describe 'debbuilder::packages::essential', :type => :class do
     "cdbs",
     "build-essential",
     "rake",
-    "git",
     "pristine-tar",
   ]
 
   mapping = [
-    { :lsbrelease    => "wheezy",  :rspec => "ruby-rspec", },
-    { :lsbrelease    => "lucid",   :rspec => "librspec-ruby", },
+    { :lsbrelease    => "wheezy",  :rspec => "ruby-rspec",    :git => "git", },
+    { :lsbrelease    => "lucid",   :rspec => "librspec-ruby", :git => "git-core", },
   ]
 
   mapping.each do |fact_mapping|
     context "with fact :lsbdistcodename set to #{fact_mapping[:lsbrelease]}" do
       let(:facts) do { :lsbdistcodename => fact_mapping[:lsbrelease], } end
 
-      (packages + [fact_mapping[:rspec]]).each do |pkg|
+      (packages + [fact_mapping[:rspec]] + [fact_mapping[:git]]).each do |pkg|
         it { should contain_package(pkg).with_ensure("present") }
       end
     end
