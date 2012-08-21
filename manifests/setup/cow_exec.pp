fab69cc2afc36e622f5d89d82c02e8257f6660a3
Matthaus Litteken <matthaus@puppetlabs.com>
2012-08-21 15:21:56 -0700

    Updating cow_exec to only build 64-bit cows on a 64-bit machine. Also updating tests.

--- manifests/setup/cow_exec.pp
+++ manifests/setup/cow_exec.pp
@@ -5,43 +5,45 @@
 # defined type also lays down two cron jobs to maintain the cows, one for each.
 
 define debbuilder::setup::cow_exec ( $cow_root = '/var/cache/pbuilder' ) {
+  if ($::architecture == "i386" or $::architecture == "amd64") {
+    exec { "${name}-i386":
+      path          => "/usr/sbin:/usr/bin:/bin:/sbin",
+      command       => "cowbuilder --create --basepath=${cow_root}/base-${title}-i386.cow/ --debug",
+      unless        => "test -e ${cow_root}/base-${name}-i386.cow",
+      environment   => ["DIST=${name}", "ARCH=i386"],
+      logoutput     => on_failure,
+      user          => root,
+      timeout       => 0,
+    }
 
-  exec { "${name}-i386":
-    path          => "/usr/sbin:/usr/bin:/bin:/sbin",
-    command       => "cowbuilder --create --basepath=${cow_root}/base-${title}-i386.cow/ --debug",
-    unless        => "test -e ${cow_root}/base-${name}-i386.cow",
-    environment   => ["DIST=${name}", "ARCH=i386"],
-    logoutput     => on_failure,
-    user          => root,
-    timeout       => 0,
+    cron { "${name}-i386":
+      command       => "cowbuilder --update --basepath=${cow_root}",
+      environment   => "DIST=${name} ARCH=i386 PATH=/usr/sbin:/usr/bin:/bin:/sbin",
+      hour          => "2",
+      minute        => "15",
+      name          => "cowbuilder update for ${name}-i386",
+      user          => root,
+    }
   }
 
-  exec { "${name}-amd64":
-    path          => "/usr/sbin:/usr/bin:/bin:/sbin",
-    command       => "cowbuilder --create --basepath=${cow_root}/base-${name}-amd64.cow/ --debug",
-    unless        => "test -e ${cow_root}/base-${name}-amd64.cow",
-    environment   => ["DIST=${name}", "ARCH=amd64"],
-    logoutput     => on_failure,
-    user          => root,
-    timeout       => 0,
-  }
+  if ($::architecture == "amd64") {
+    exec { "${name}-amd64":
+      path          => "/usr/sbin:/usr/bin:/bin:/sbin",
+      command       => "cowbuilder --create --basepath=${cow_root}/base-${name}-amd64.cow/ --debug",
+      unless        => "test -e ${cow_root}/base-${name}-amd64.cow",
+      environment   => ["DIST=${name}", "ARCH=amd64"],
+      logoutput     => on_failure,
+      user          => root,
+      timeout       => 0,
+    }
 
-  cron { "${name}-i386":
-    command       => "cowbuilder --update --basepath=${cow_root}",
-    environment   => "DIST=${name} ARCH=i386 PATH=/usr/sbin:/usr/bin:/bin:/sbin",
-    hour          => "2",
-    minute        => "15",
-    name          => "cowbuilder update for ${name}-i386",
-    user          => root,
+    cron { "${name}-amd64":
+      command       => "cowbuilder --update --basepath=${cow_root}",
+      environment   => "DIST=${name} ARCH=amd64 PATH=/usr/sbin:/usr/bin:/bin:/sbin",
+      hour          => "2",
+      minute        => "15",
+      name          => "cowbuilder update for ${name}-amd64",
+      user          => root,
+    }
   }
-
-  cron { "${name}-amd64":
-    command       => "cowbuilder --update --basepath=${cow_root}",
-    environment   => "DIST=${name} ARCH=amd64 PATH=/usr/sbin:/usr/bin:/bin:/sbin",
-    hour          => "2",
-    minute        => "15",
-    name          => "cowbuilder update for ${name}-amd64",
-    user          => root,
-  }
-
 }
--- spec/defines/setup_cow_exec_spec.rb
+++ spec/defines/setup_cow_exec_spec.rb
@@ -19,27 +19,35 @@ describe 'debbuilder::setup::cow_exec', :type => :define do
       let :params do
         param_set
       end
+      arches = ['i386', 'amd64']
 
-      ['i386','amd64'].each do | arch |
-        it do should contain_exec("#{title}-#{arch}").with( {
-            :path         => "/usr/sbin:/usr/bin:/bin:/sbin",
-            :command      => "cowbuilder --create --basepath=#{param_hash[:cow_root]}/base-#{title}-#{arch}.cow/ --debug",
-            :unless       => "test -e #{param_hash[:cow_root]}/base-#{title}-#{arch}.cow",
-            :environment  => ["DIST=#{title}", "ARCH=#{arch}"],
-            :logoutput    => "on_failure",
-            :user         => "root",
-            :timeout      => "0",
-          } )
-        end
+      arches.each do | arch_fact |
+        context "on #{arch_fact}" do
+          let(:facts) do { :architecture => arch_fact } end
+          arches.each do | arch |
+            if (arch_fact == "amd64") or (arch_fact == arch)
+              it do should contain_exec("#{title}-#{arch}").with( {
+                  :path         => "/usr/sbin:/usr/bin:/bin:/sbin",
+                  :command      => "cowbuilder --create --basepath=#{param_hash[:cow_root]}/base-#{title}-#{arch}.cow/ --debug",
+                  :unless       => "test -e #{param_hash[:cow_root]}/base-#{title}-#{arch}.cow",
+                  :environment  => ["DIST=#{title}", "ARCH=#{arch}"],
+                  :logoutput    => "on_failure",
+                  :user         => "root",
+                  :timeout      => "0",
+                } )
+              end
 
-        it do should contain_cron("#{title}-#{arch}").with({
-            :command      => "cowbuilder --update --basepath=#{param_hash[:cow_root]}",
-            :environment  => "DIST=#{title} ARCH=#{arch} PATH=/usr/sbin:/usr/bin:/bin:/sbin",
-            :hour         => "2",
-            :minute       => "15",
-            :user         => "root",
-            :name         => "cowbuilder update for #{title}-#{arch}",
-          })
+              it do should contain_cron("#{title}-#{arch}").with({
+                  :command      => "cowbuilder --update --basepath=#{param_hash[:cow_root]}",
+                  :environment  => "DIST=#{title} ARCH=#{arch} PATH=/usr/sbin:/usr/bin:/bin:/sbin",
+                  :hour         => "2",
+                  :minute       => "15",
+                  :user         => "root",
+                  :name         => "cowbuilder update for #{title}-#{arch}",
+                })
+              end
+            end
+          end
         end
       end
     end
