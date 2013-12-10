05b40ca4d6c5280ca21baab6a61de71f0a8579cc
Moses Mendoza <moses@puppetlabs.com>
2013-12-10 15:45:57 -0800

    Allow upstream mirrors for pbuilderrc to be configurable
    This commit adds configurability of the debian and ubuntu mirrors to use when
    creating the pbuilderrc file. This way, if you have a custom internal debian
    mirror, you can supply it in your debbuilder inclusion and use it to satisfy
    deps for cows instead of the official debian/ubuntu mirrors.
    
    Signed-off-by: Moses Mendoza <moses@puppetlabs.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,13 +3,19 @@
 # for internal Puppet Labs builds, $use_cows sets up cows as builders and
 # brings in all the dependencies to set cows up. $cows and $cow_root customize
 # which cows are build during the setup phase and which basepath to use when
-# setting the cows up (if $use_cows is set to true).
+# setting the cows up (if $use_cows is set to true). $debian_mirror,
+# $debian_archive_mirror, and $ubuntu_mirror can be used to specify the mirrors
+# that will be used by pbuilder/cowbuilder during dependency resolution/OS
+# build.
 
 class debbuilder (
   $pe = false,
   $use_cows = false,
   $cows = undef,
   $cow_root = undef,
+  $debian_mirror = 'ftp.us.debian.org',
+  $debian_archive_mirror = 'archive.debian.org',
+  $ubuntu_mirror = 'us.archive.ubuntu.com',
 ) {
   include debbuilder::packages::essential
 
